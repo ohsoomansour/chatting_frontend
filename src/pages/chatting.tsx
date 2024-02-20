90b60029328706ce391f91c11eaf3f2002424139
ohsoomansour <ceoosm@naver.com>
2024-02-20 15:04:38 +0900

    trade

--- src/pages/chatting.tsx
+++ src/pages/chatting.tsx
@@ -51,7 +51,7 @@ interface IProps {
   time: string;
 }
 export const WS_BASE_PATH = process.env.NODE_ENV === "production" 
- ? "wss://trade-2507d8197825.herokuapp.com"
+ ? "wss://trade.herokuapp.com"
  : "http://localhost:8080";
 
 
